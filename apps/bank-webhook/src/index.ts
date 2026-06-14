8a05784d69b567984dec01a4a77ce302af3b2ac7
Rohit-Gahlawat <283466839+Rohit-Gahlawat@users.noreply.github.com>
2026-06-15 01:37:50 +0530

    bankWebHook_added

---
+++ apps/bank-webhook/src/index.ts
@@ -0,0 +1,52 @@
+import express from "express";
+import db from "@repo/db"
+const app = express();
+app.use(express.json());
+
+
+app.post("", async (req, res) => {
+
+    const paymentInfo = {
+        token: req.body.token,
+        userId: req.body.user_identifier,
+        amount: req.body.amount
+    }
+    try {
+        await db.$transaction([
+            db.balance.update({
+                where: {
+                    userId: (paymentInfo.userId)
+                },
+                data: {
+                    amount: {
+
+                        increment: Number(paymentInfo.amount)
+                    }
+                }
+            }),
+            db.onRampTransaction.update({
+                where: {
+                    token: paymentInfo.token
+                },
+                data: {
+                    status: "Success",
+                }
+            })
+        ]);
+
+        res.json({
+            message: "Captured"
+        })
+    } catch (e) {
+        console.log(e);
+        res.status(400).json({
+            message: "Error while processing webhook"
+        })
+    }
+
+})
+
+
+
+
+app.listen(3000, () => console.log("bank-webhook server is ruinning on port 3003"));
